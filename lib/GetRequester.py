413cbacfd5d16d4eeb6bd237178a55e1f602613c
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-06-22 07:57:35 +0300

    Getting Remote Data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,16 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
+    
+# THis prints the data in our terminal as json data
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+people_data = get_requester.load_json()
+print(people_data)
+
+# But the printed data is not being indented 
